b0ce50b23a0514537852eef28970eb1add2def61
YvesThirion <thirion.yves@gmail.com>
2021-10-28 15:13:29 +0200

    Enhanced sizes & positions buttons
    Use relative vw of screen

--- src/client/components/homepage.jsx
+++ src/client/components/homepage.jsx
@@ -18,7 +18,7 @@ const Homepage = () => {
 export default Homepage;
 
 const Title = styled.h1`
-  margin: 50px;
+  margin-top: 100px;
   width: max-content;
   margin-right: auto;
   margin-left: auto;
@@ -32,20 +32,22 @@ const Wrapper = styled.section`
   /**
   * Fit components in wrapper from their sizes (grid of 4 buttons)
   * Total wrapper width = width * 2 + margin * 4
+  * 80 = 30 * 2 + 5 * 4
   */
-  width: 280px;
+  width: 80vw;
   margin-left: auto;
   margin-right: auto;
   background-color: #d9ff00;
-  -ms-transform: translateY(25vh);
-  transform: translateY(25vh);
+  transform: translateY(10vh);
 `;
 
 const Tile = styled.button`
   background-color: #2f00ff;
-  width: 100px;
-  height: 100px;
+  width: 30vw;
+  height: 200px;
   margin: 20px;
+  margin-left: 5vw;
+  margin-right: 5vw;
   border-radius: 5px;
   border: solid 1px green;
   font-size: 1em;
