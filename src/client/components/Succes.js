7e4556a77a214f32e96ec3be4c9a5922048dd9bc
corentinIstace <corentinistace1@gmail.com>
2021-10-28 16:53:11 +0200

    create succes component and display it

---
+++ src/client/components/Succes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Succes = () => {
+    return (
+        <Container>
+            <p>SUCCES</p>
+        </Container>
+    )
+}
+
+const Container = styled.div`
+    background-color: rebeccapurple;
+    width: 200px;
+    height: 150px;
+`;
+
+export default Succes
