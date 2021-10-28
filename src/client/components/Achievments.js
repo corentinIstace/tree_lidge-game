b0e2f11555348106053a2ef28e8a8afbcd9dda04
corentinIstace <corentinistace1@gmail.com>
2021-10-28 16:52:41 +0200

    create achievment component and display it

---
+++ src/client/components/Achievments.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Achievments = () => {
+    return (
+        <Container>
+            <p>ACHIEVMENTS</p>
+        </Container>
+    )
+}
+
+const Container = styled.div`
+    background-color: rebeccapurple;
+    width: 200px;
+    height: 150px;
+`;
+export default Achievments
