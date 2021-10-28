6160246d222830b32baee0966231904d0d0ae069
corentinIstace <corentinistace1@gmail.com>
2021-10-28 16:52:15 +0200

    create user achievment component and display it

---
+++ src/client/components/UserAchievments.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const UserAchievments = () => {
+    return (
+        <Container>
+            <p>USER ACHIEVMENTS</p>
+        </Container>
+    )
+}
+
+const Container = styled.div`
+    background-color: rebeccapurple;
+    width: 450px;
+    height: 200px;
+`;
+export default UserAchievments;
