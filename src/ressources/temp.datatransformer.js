ba8407adb03f24bbbe1de7fd0657699be4d34c65
Yves <thirion.yves@gmail.com>
2021-11-16 09:22:07 +0100

    Updating values, base on size and circonference

--- src/ressources/temp.datatransformer.js
+++ src/ressources/temp.datatransformer.js
@@ -15,18 +15,22 @@
 import Trees from "./arbustum.json";
 
 export const Collection = () => {
+    console.log("raw dataset", Trees);
     console.log(
         "resulting data set",
-        Trees.map(tree => {
-            const newTree = {
-                geoloc: tree.geoloc,
-                value: tree.hauteur_totale * tree.diametre_cime,
-                name: tree.nom_complet,
-                owner: null,
-                isLocked: false,
-            };
-            return newTree;
-        }),
+        // Filter out trees missing both size and circonference values
+        Trees.filter(tree => tree.hauteur_totale > 0 && tree.circonf > 0).map(
+            tree => {
+                const newTree = {
+                    geoloc: tree.geoloc,
+                    value: tree.hauteur_totale * (tree.circonf / Math.PI),
+                    name: tree.nom_complet,
+                    owner: null,
+                    isLocked: false,
+                };
+                return newTree;
+            },
+        ),
     );
     return null;
 };
