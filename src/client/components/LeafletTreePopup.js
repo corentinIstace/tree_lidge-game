2073c0463130e52103ddb14f3f7717617349a317
Yves <thirion.yves@gmail.com>
2021-11-16 10:32:46 +0100

    Added conditionnal display and buttons for tree markup

--- src/client/components/LeafletTreePopup.js
+++ src/client/components/LeafletTreePopup.js
@@ -4,21 +4,39 @@ const TreePopup = props => {
     const Tree = props.tree;
     return (
         <>
-            {Tree.name}
+            <b>{Tree.name}</b>
             <br />
             {`Price : ${Tree.value ?? "None"}`}
             <br />
-            {Tree.owner == null ? (
-                <button
-                    type={"button"}
-                    onClick={() => {
-                        console.log("Buy tree", Tree.name);
-                    }}>
-                    {"Buy tree"}
-                </button>
-            ) : (
-                `Owned by ${Tree.owner}`
-            )}
+            <br />
+            {Tree.owner ? `Owned by ${Tree.owner}` : `Free tree`}
+            <br />
+
+            {
+                // If tree not owned by user, display button
+                Tree.owner !== props.userName && !Tree.isLocked ? (
+                    <button
+                        type={"button"}
+                        onClick={() => {
+                            // TODO db request buying tree
+                            console.log("Buy tree", Tree.name);
+                        }}>
+                        {"Buy tree"}
+                    </button>
+                ) : // If not owned but locked
+                Tree.owner !== props.userName && Tree.isLocked ? (
+                    "Locked"
+                ) : (
+                    <button
+                        type={"button"}
+                        onClick={() => {
+                            // TODO db request buying tree
+                            console.log("Lock tree", Tree.name);
+                        }}>
+                        {"Lock tree"}
+                    </button>
+                )
+            }
         </>
     );
 };
