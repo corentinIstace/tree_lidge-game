015ec0f5db76115b08e0630c6ac081988322a183
LucasIelli <lucas.ielli.pro@gmail.com>
2021-11-23 10:31:14 +0100

    HerokuUrl.js need export default.

--- client/src/HerokuUrl.js
+++ client/src/HerokuUrl.js
@@ -1,5 +1,5 @@
 const SERVER_PORT = process.env.PORT || 5000;
 
-const HerokuUrl = `https://treelidgegame.herokuapp.com:${SERVER_PORT}/`;
+const HerokuUrl = `https://treelidgegame.herokuapp.com:${SERVER_PORT}`;
 
 export default HerokuUrl;
