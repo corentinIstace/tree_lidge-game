309df946205e178b2602801d46bf5e3340bbae0d
LucasIelli <lucas.ielli.pro@gmail.com>
2021-11-23 11:49:47 +0100

    add herokuUrl in App.js for all the routes

--- client/src/App.js
+++ client/src/App.js
@@ -6,19 +6,23 @@ import Gamepage from "./containers/Gamepage.js";
 import UserPage from "./containers/UserPage.js";
 import NotFound from "./components/NotFound.js";
 import LeaderboardPage from "./containers/LeaderboardPage.js";
+import HerokuUrl from "./HerokuUrl.js";
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path={"/"} exact element={<Logpage />} />
-                <Route path={"/login"} element={<Logpage />} />
-                <Route path={"/register"} element={<Logpage />} />
-                <Route path={"/gamepage"} element={<Gamepage />} />
-                <Route path={"/homepage"} element={<Homepage />} />
-                <Route path={"/userpage"} element={<UserPage />} />
-                <Route path={"/leaderboard"} element={<LeaderboardPage />} />
-                <Route path={"*"} element={<NotFound />} />
+                <Route path={`${HerokuUrl}/`} exact element={<Logpage />} />
+                <Route path={`${HerokuUrl}/login`} element={<Logpage />} />
+                <Route path={`${HerokuUrl}/register`} element={<Logpage />} />
+                <Route path={`${HerokuUrl}/gamepage`} element={<Gamepage />} />
+                <Route path={`${HerokuUrl}/homepage`} element={<Homepage />} />
+                <Route path={`${HerokuUrl}/userpage`} element={<UserPage />} />
+                <Route
+                    path={`${HerokuUrl}/leaderboard`}
+                    element={<LeaderboardPage />}
+                />
+                <Route path={`${HerokuUrl}/*`} element={<NotFound />} />
             </Routes>
         </Router>
     );
